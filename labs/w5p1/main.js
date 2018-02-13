72eae9d25a47bcdfaad1dd87957f1dfd9e50f4f6
Amy Lin <adlin@andrew.cmu.edu>
2018-02-13 11:26:20 -0500

    finished lab

--- labs/w5p1/main.js
+++ labs/w5p1/main.js
@@ -1,4 +1,8 @@
 function addNewList() {
-    alert('hello world alert!');
-    console.log('hello world console');
+    var list = document.getElementById("grocery-list");
+    var itemInput = document.getElementById("new-list-item");
+    var newItem = document.createElement("li");
+    newItem.appendChild(document.createTextNode(itemInput.value));
+    list.appendChild(newItem);
 }
+
